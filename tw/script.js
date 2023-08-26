053cdf64f49affb8c072d9aa38fcccc4ff2506c1
taylortwisun <40511805+taylortwisun@users.noreply.github.com>
2023-08-26 09:21:23 +0700

    Create script.js
    file js

---
+++ tw/script.js
@@ -0,0 +1,13 @@
+const background = document.getElementById("background");
+const menuItems = document.querySelectorAll(".menu li a");
+
+function setBackground() {
+    // ... (code from previous example)
+}
+
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    menu.classList.toggle("show");
+}
+
+setBackground();
