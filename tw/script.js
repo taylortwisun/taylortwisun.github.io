7abeedeccecbdfc7d1f1d107819555b06e18d3e5
taylortwisun <40511805+taylortwisun@users.noreply.github.com>
2023-08-26 09:25:07 +0700

    Update script.js
    file js update full

--- tw/script.js
+++ tw/script.js
@@ -2,12 +2,56 @@ const background = document.getElementById("background");
 const menuItems = document.querySelectorAll(".menu li a");
 
 function setBackground() {
-    // ... (code from previous example)
+    const now = new Date();
+    const currentMonth = now.getMonth() + 1;
+
+    let season = "";
+    let effect = "";
+
+    if (currentMonth >= 3 && currentMonth <= 5) {
+        season = "spring";
+        effect = "none";
+    } else if (currentMonth >= 6 && currentMonth <= 8) {
+        season = "summer";
+        effect = "none";
+    } else if (currentMonth >= 9 && currentMonth <= 11) {
+        season = "autumn";
+        effect = "falling-leaves";
+    } else {
+        season = "winter";
+        effect = "snow";
+    }
+
+    background.style.backgroundImage = `url('${season}.jpg')`;
+    background.style.backgroundSize = "cover";
+    background.style.backgroundPosition = "center";
+
+    menuItems.forEach(item => {
+        if (item.textContent === "Trang Chủ") {
+            item.classList.add("active");
+        } else {
+            item.classList.remove("active");
+        }
+    });
+
+    if (effect === "falling-leaves") {
+        createFallingLeaves();
+    } else if (effect === "snow") {
+        createFallingSnow();
+    }
 }
 
 function toggleMenu() {
     var menu = document.getElementById("menu");
     menu.classList.toggle("show");
+}
+
+function createFallingLeaves() {
+    // Thêm mã JavaScript cho hiệu ứng lá rơi ở đây
+}
+
+function createFallingSnow() {
+    // Thêm mã JavaScript cho hiệu ứng tuyết rơi ở đây
 }
 
 setBackground();
